32aff059388302d05619bb0ab623fbc1ea9c8ef7
Yassir <yassirscreed@hotmail.com>
2022-01-11 15:24:03 +0000

    1650

--- projeto1/proj1.cpp
+++ projeto1/proj1.cpp
@@ -2,147 +2,146 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
-
 
 int problema1();
 int problema2();
-int find_smaller_map(map<int, vector<int> > comp_val, int comp, int value);
+void insertSorted(vector<int> &v, int n, int key);
 
-int main(){
+int main()
+{
     int nprob;
     cin >> nprob;
-    if(nprob == 1){
+    if (nprob == 1)
+    {
         problema1();
     }
-    else if (nprob == 2){
+    else if (nprob == 2)
+    {
         problema2();
     }
     return 0;
 }
 
-int problema1(){
+int problema1()
+{
     vector<int> s(0);
-    int i,j;
-    // ler o input 
-    while(cin >> i){
+    int i, j;
+    // ler o input
+    while (cin >> i)
+    {
         s.push_back(i);
-        if (cin.get() == '\n'){
+        if (cin.get() == '\n')
+        {
             break;
         }
     }
-    //algoritmo
+    // algoritmo
     int size_s = s.size();
     // caso base
-    if (size_s == 1){
+    if (size_s == 1)
+    {
         cout << "1 1" << endl;
         return 0;
     }
     vector<int> comps(size_s);
-    vector<int> numbersubs(size_s,1);
-    comps[0] = 1;    
-    for(i=1 ; i < size_s; i++){
-        //ir metendo a entrada do vetor comp a 1
+    vector<int> numbersubs(size_s, 1);
+    comps[0] = 1;
+    for (i = 1; i < size_s; i++)
+    {
+        // ir metendo a entrada do vetor comp a 1
         comps[i] = 1;
-        for(j = 0; j < i; j++){
-            if(s[j] < s[i]){
-                if(comps[i] < comps[j] + 1){
-                    comps[i] = comps[j] + 1; 
+        for (j = 0; j < i; j++)
+        {
+            if (s[j] < s[i])
+            {
+                if (comps[i] < comps[j] + 1)
+                {
+                    comps[i] = comps[j] + 1;
                     numbersubs[i] = numbersubs[j];
                 }
-                // caso em que existe masi uma subsequencia
-                else if(comps[i] == comps[j] + 1)
+                // caso em que existe mais uma subsequencia
+                else if (comps[i] == comps[j] + 1)
                     numbersubs[i] += numbersubs[j];
             }
         }
     }
     int lenght = 0;
-    //descobrir o comprimento maximo
-    for(i = 0; i < size_s; i++){
-        if(comps[i] > lenght)
+    // descobrir o comprimento maximo
+    for (i = 0; i < size_s; i++)
+    {
+        if (comps[i] > lenght)
             lenght = comps[i];
     }
     int num_subs = 0;
-    //descobrir o numero de subsequencias
-    for(i = 0; i < size_s; i++){
-        if(comps[i] == lenght)
+    // descobrir o numero de subsequencias
+    for (i = 0; i < size_s; i++)
+    {
+        if (comps[i] == lenght)
             num_subs += numbersubs[i];
     }
     cout << lenght << " " << num_subs << endl;
     return 0;
 }
 
-int problema2(){
+int problema2()
+{
     vector<int> x(0);
     vector<int> y(0);
-    int i;
-    //ler o input 
-    while(cin >> i){
+    unordered_map<int, int> aux;
+    int i, j;
+
+    // ler o input
+
+    while (cin >> i)
+    {
+        aux[i] = 1;
         x.push_back(i);
-        if (cin.get() == '\n'){
+        if (cin.get() == '\n')
+        {
             break;
         }
     }
-    while(cin >> i){
-        y.push_back(i);
-        if (cin.get() == '\n'){
+
+    while (cin >> i)
+    {
+        int flag = 0;
+        if (aux.find(i) == aux.end())
+            flag = 1;
+        if (flag == 0)
+            y.push_back(i);
+        if (cin.get() == '\n')
+        {
             break;
         }
     }
-    //algoritmo
+
     int size_x = x.size(), size_y = y.size();
-    vector<vector<int>> matrix;
 
-    //coluna 0 a 0's
-    for(int i=0; i <= size_x; i++){
-        matrix.push_back(vector<int>());
-        matrix[i].push_back(0);    
-    }
+    int vec_res[size_y];
+    for (i = 0; i < size_y; i++)
+        vec_res[i] = 0;
 
-    //linha 0 a 0's
-    for(i=0;i<=size_y;i++){
-        matrix[0].push_back(0);
-    }
-
-    map<int, vector<int> > comp_val;
-    comp_val[0].push_back(0);
-
-    //preencher a matriz com o algoritmo
-    for(int i=1; i <= size_x; i++){
-        for(int j=1; j <= size_y; j++){
-            if(x[i-1] == y[j-1]){
-                if(find_smaller_map(comp_val,matrix[i-1][j-1],x[i-1]) == 1){
-                    matrix[i].push_back(matrix[i-1][j-1] + 1);
-                    //meter no mapa com key = comp, o numero
-                    comp_val[matrix[i][j]].push_back(x[i-1]);
-                }
-                else
-                    matrix[i].push_back(matrix[i-1][j-1]);
-            }
-            else
-                matrix[i].push_back(max(matrix[i][j-1], matrix[i-1][j]));
+    // para cada elemento de x, vai percorrer o vetor de y
+    for (i = 0; i < size_x; i++)
+    {
+        int cur = 0;
+        for (j = 0; j < size_y; j++)
+        {
+            if (x[i] == y[j])
+                vec_res[j] = max(vec_res[j], cur + 1);
+            if (y[j] < x[i] && vec_res[j] > cur)
+                cur = vec_res[j];
         }
     }
+    // ir buscar ao vetor o maior comprimento
+    int max_len = 0;
+    for (i = 0; i < size_y; i++)
+    {
+        max_len = max(max_len, vec_res[i]);
+    }
 
-    /*for(int i = 0; i<=size_x; i++){
-        for(int j = 0; j<=size_y; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }*/
-
-    cout << matrix[size_x][size_y] << endl;
-
+    cout << max_len << endl;
     return 0;
-}
-
-int find_smaller_map(map<int, vector<int> > comp_val, int comp, int value){
-    //se existe no map vamos ver se ha valor menor
-    if (comp_val.count(comp) == 1){
-        int i = *min_element(comp_val[comp].begin(), comp_val[comp].end());
-        if(i < value)
-            return 1;
-        else return 0;
-    }
-    return 1;
 }
